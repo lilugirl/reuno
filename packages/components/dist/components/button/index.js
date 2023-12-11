cdf7866beb7461cb482dea57625c7fbbafc02e7f
liuyi <yi7.liu@symbio.com>
2023-12-11 22:34:18 +0800

    init

---
+++ packages/components/dist/components/button/index.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.Button = void 0;
+var react_1 = require("react");
+var Button = function () {
+    return react_1.default.createElement("div", null, "button");
+};
+exports.Button = Button;
